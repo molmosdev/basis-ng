b43d4df5aa9f7e6b1edaea47d46411b9ae426875
molmos <molmos@circontrol.com>
2025-04-20 19:34:30 +0200

    fix(introduction): correct styleUrl to styleUrls in IntroductionComponent and enhance introduction content

--- projects/docs/src/app/features/introduction/introduction.component.ts
+++ projects/docs/src/app/features/introduction/introduction.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'article[app-introduction]',
   templateUrl: './introduction.component.html',
-  styleUrl: './introduction.component.css',
+  styleUrls: ['./introduction.component.css'],
 })
 export default class IntroductionComponent {}
