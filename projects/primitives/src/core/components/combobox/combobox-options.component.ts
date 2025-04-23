2f8a092f6ead572b48b76b73b5969c11285eaac8
molmosdev <hello@molmos.dev>
2025-04-23 20:42:54 +0200

    feat: implement Combobox and ComboboxOptions components for enhanced dropdown functionality

---
+++ projects/primitives/src/core/components/combobox/combobox-options.component.ts
@@ -0,0 +1,83 @@
+import { CdkListbox } from '@angular/cdk/listbox';
+import {
+  Component,
+  contentChildren,
+  ElementRef,
+  inject,
+  input,
+  output,
+  signal,
+} from '@angular/core';
+import { OptionComponent } from '../../../shared/components/option.component';
+import { Input } from '@basis-ng/primitives';
+
+/**
+ * Component representing the list of options in a combobox.
+ * This component integrates with Angular CDK Listbox to manage options and their selection.
+ */
+@Component({
+  selector: 'ul[b-combobox-options]',
+  imports: [Input],
+  template: `<input type="text" b-input /> <ng-content />`,
+  hostDirectives: [
+    {
+      directive: CdkListbox,
+      inputs: ['cdkListboxValue'],
+      outputs: ['cdkListboxValueChange'],
+    },
+  ],
+  host: {
+    '[cdkListboxValue]': 'value()',
+    '(cdkListboxValueChange)': 'handleValueChange($event.value)',
+    '[style.max-height]': 'maxHeight()',
+  },
+})
+export class ComboboxOptionsComponent {
+  /**
+   * Signal representing the selected values in the listbox.
+   * This is an array of strings corresponding to the selected option values.
+   */
+  readonly value = signal<string[]>([]);
+
+  /**
+   * Event emitter triggered when the dropdown should close.
+   * This is used to notify the parent component to close the dropdown.
+   */
+  closeEmitter = output();
+
+  /**
+   * Reference to the host element of the component.
+   * This provides access to the DOM element of the options list.
+   */
+  el = inject(ElementRef);
+
+  /**
+   * Input for setting the maximum height of the dropdown.
+   * Defaults to '300px'. This controls the vertical size of the dropdown.
+   */
+  readonly maxHeight = input<string>('300px');
+
+  /**
+   * Reference to the CDK Listbox directive.
+   * This is used to manage the options and their selection state.
+   */
+  listBox = inject(CdkListbox);
+
+  /**
+   * Reference to the list of options in the dropdown.
+   * This is a collection of `OptionComponent` instances representing the available options.
+   */
+  readonly options = contentChildren(OptionComponent);
+
+  /**
+   * Handles changes to the selected value in the listbox.
+   * This method updates the `value` signal, emits the `closeEmitter` event,
+   * and ensures the parent component is notified of the selection change.
+   *
+   * @param value - The new array of selected values.
+   */
+  handleValueChange(value: string[]) {
+    this.value.set(value);
+    this.closeEmitter.emit();
+  }
+}
---
+++ projects/primitives/src/core/components/combobox/combobox.component.ts
@@ -0,0 +1,266 @@
+import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
+import {
+  Component,
+  computed,
+  contentChild,
+  input,
+  linkedSignal,
+  model,
+  OnInit,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { Button } from '../button/button.component';
+import { Icon } from '../icon/icon.component';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { forwardRef } from '@angular/core';
+import { ComboboxOptionsComponent } from './combobox-options.component';
+
+/**
+ * Component representing a combobox dropdown.
+ * This component provides a button to toggle the dropdown and displays the selected option(s) with an input field.
+ */
+@Component({
+  selector: 'b-combobox',
+  imports: [Button, Icon, CdkConnectedOverlay, CdkOverlayOrigin],
+  template: ` <button
+      b-button
+      variant="outlined"
+      (click)="isOpen() ? close() : open()"
+      (keydown.arrowUp)="!isOpen() && open()"
+      (keydown.arrowDown)="!isOpen() && open()"
+      cdkOverlayOrigin
+      [activeEnabled]="false"
+      #trigger="cdkOverlayOrigin">
+      {{ content() }}
+      <i b-icon icon="ChevronDown" [size]="20"></i>
+    </button>
+    <ng-template
+      cdkConnectedOverlay
+      [cdkConnectedOverlayOrigin]="trigger"
+      [cdkConnectedOverlayOpen]="isOpen()"
+      [cdkConnectedOverlayWidth]="buttonWidth()"
+      [cdkConnectedOverlayHasBackdrop]="true"
+      cdkConnectedOverlayBackdropClass="cdk-overlay-transparent-backdrop"
+      [cdkConnectedOverlayPositions]="[
+        {
+          originX: 'start',
+          originY: 'bottom',
+          overlayX: 'start',
+          overlayY: 'top',
+          offsetY: 5,
+        },
+        {
+          originX: 'start',
+          originY: 'top',
+          overlayX: 'start',
+          overlayY: 'bottom',
+          offsetY: -5,
+        },
+      ]"
+      (backdropClick)="close()"
+      (detach)="close()">
+      <ng-content />
+    </ng-template>`,
+  host: {
+    '[style.max-width]': 'maxWidth()',
+    '[class.disabled]': 'disabled()',
+  },
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ComboboxComponent),
+      multi: true,
+    },
+  ],
+})
+export class ComboboxComponent implements OnInit, ControlValueAccessor {
+  /**
+   * Placeholder text displayed when no option is selected.
+   * Defaults to 'Select an option'.
+   */
+  readonly placeholder = input<string>('Select an option');
+
+  /**
+   * Signal indicating whether the dropdown is currently open.
+   */
+  readonly isOpen = signal(false);
+
+  /**
+   * Reference to the button element used to toggle the dropdown.
+   * This is used for managing focus and interactions.
+   */
+  readonly button = viewChild(Button);
+
+  /**
+   * Reference to the content component of the dropdown.
+   * This contains the list of selectable options.
+   */
+  readonly comboboxOptions = contentChild(ComboboxOptionsComponent);
+
+  /**
+   * Computed signal representing the selected values from the dropdown.
+   * This is linked to the value of the `ComboboxOptionsComponent`.
+   */
+  readonly value = linkedSignal(() => this.comboboxOptions()?.value());
+
+  /**
+   * Input for setting the maximum width of the dropdown.
+   * Defaults to '100%'.
+   */
+  readonly maxWidth = input('100%');
+
+  /**
+   * Computed signal for the width of the button element.
+   * This is used to set the width of the dropdown overlay.
+   */
+  readonly buttonWidth = computed(
+    () => this.button()?.el.nativeElement.offsetWidth
+  );
+
+  /**
+   * Model indicating whether the combobox component is disabled.
+   * When disabled, the dropdown cannot be opened or interacted with.
+   */
+  readonly disabled = model(false);
+
+  /**
+   * Computed signal representing the options available in the dropdown.
+   * This retrieves the options from the `ComboboxOptionsComponent`.
+   */
+  readonly options = computed(() => this.comboboxOptions()?.options());
+
+  /**
+   * Computed signal representing the content of the selected option(s).
+   * If no option is selected, it returns the placeholder text.
+   */
+  readonly content = computed(() => {
+    const selected = this.value();
+    if (selected && selected.length > 0) {
+      return this.options()?.reduce((acc, option) => {
+        if (selected.includes(option.cdkOption.value)) {
+          return acc
+            ? acc + ', ' + option.el.nativeElement.innerText
+            : option.el.nativeElement.innerText;
+        }
+        return acc;
+      }, '');
+    } else {
+      return this.placeholder();
+    }
+  });
+
+  /**
+   * Signal representing the delay before closing the dropdown.
+   * This is used to provide a smooth transition when closing the dropdown.
+   */
+  readonly closeDelay = signal(150);
+
+  /**
+   * Reference to the CdkConnectedOverlay directive.
+   * This is used to manage the positioning and visibility of the dropdown overlay.
+   */
+  readonly cdkConnectedOverlay = viewChild(CdkConnectedOverlay);
+
+  /**
+   * Lifecycle hook that is called after the component is initialized.
+   * It sets up the necessary subscriptions for handling value changes.
+   */
+  ngOnInit(): void {
+    this.handleSelectedValueChange();
+  }
+
+  /**
+   * Subscribes to the `closeEmitter` of the `ComboboxOptionsComponent` to handle
+   * changes to the selected value. This ensures the dropdown closes and the
+   * value is propagated to Angular Forms.
+   */
+  handleSelectedValueChange() {
+    this.comboboxOptions()?.closeEmitter.subscribe(() => {
+      this.onChange(this.value()!); // Notify Angular Forms about the change
+      this.onTouched(); // Mark the component as touched
+      this.close(); // Close the dropdown
+    });
+  }
+
+  /**
+   * Opens the dropdown and focuses the listbox.
+   * This method sets the `isOpen` signal to `true` and ensures the listbox gains focus.
+   */
+  open() {
+    this.isOpen.set(true);
+    setTimeout(() => this.comboboxOptions()?.el.nativeElement.focus(), 0);
+  }
+
+  /**
+   * Closes the dropdown with a transition effect and refocuses the button.
+   * This method sets the `isOpen` signal to `false` after a delay and removes
+   * the transition class from the overlay panel.
+   */
+  close() {
+    this.cdkConnectedOverlay()?.overlayRef.addPanelClass(
+      'cdk-overlay-pane-closing'
+    );
+
+    setTimeout(() => {
+      this.isOpen.set(false);
+      this.cdkConnectedOverlay()?.overlayRef.removePanelClass(
+        'cdk-overlay-pane-closing'
+      );
+      this.button()?.el.nativeElement.focus();
+    }, this.closeDelay());
+  }
+
+  // Control Value Accessor methods
+
+  /**
+   * Callback function to propagate changes to the model.
+   * This is called whenever the value changes.
+   */
+  private onChange: (value: string[]) => void = () => undefined;
+
+  /**
+   * Callback function to mark the component as touched.
+   * This is called when the component loses focus.
+   */
+  private onTouched: () => void = () => undefined;
+
+  /**
+   * Writes a new value to the component.
+   * This method is called by Angular Forms to update the value of the combobox component.
+   * @param value - The new value to set.
+   */
+  writeValue(value: string[]): void {
+    if (value) {
+      value.forEach(value => {
+        this.comboboxOptions()?.listBox?.selectValue(value);
+      });
+      this.value.set(value);
+    }
+  }
+
+  /**
+   * Registers a callback function to be called when the value changes.
+   * @param fn - The callback function.
+   */
+  registerOnChange(fn: (value: string[]) => void): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registers a callback function to be called when the component is touched.
+   * @param fn - The callback function.
+   */
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Sets the disabled state of the component.
+   * This method is called by Angular Forms to enable or disable the component.
+   * @param isDisabled - A boolean indicating whether the component should be disabled.
+   */
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+}
