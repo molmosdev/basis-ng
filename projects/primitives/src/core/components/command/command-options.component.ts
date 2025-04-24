aa9dccf4d5525b5b45af5e3f690b3bcec1bb41d6
molmos <molmos@circontrol.com>
2025-04-25 00:07:30 +0200

    feat: add CommandComponent and CommandOptionsComponent for enhanced command input functionality

---
+++ projects/primitives/src/core/components/command/command-options.component.ts
@@ -0,0 +1,98 @@
+import { ActiveDescendantKeyManager } from '@angular/cdk/a11y';
+import { CdkListbox, CdkOption } from '@angular/cdk/listbox';
+import {
+  Component,
+  computed,
+  contentChildren,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+
+/**
+ * Component representing a list of command options.
+ * It uses Angular CDK's listbox and option utilities for accessibility and keyboard navigation.
+ */
+@Component({
+  selector: 'ul[b-command-options]',
+  imports: [],
+  template: `<ng-content />`,
+  hostDirectives: [CdkListbox],
+})
+export class CommandOptionsComponent implements OnInit {
+  /**
+   * Collection of child options within the listbox.
+   */
+  readonly options = contentChildren(CdkOption);
+
+  /**
+   * Key manager for handling keyboard navigation and active descendant management.
+   */
+  readonly listKeyManager = computed(() =>
+    new ActiveDescendantKeyManager(this.options())
+      .withWrap()
+      .withVerticalOrientation()
+  );
+
+  /**
+   * Signal representing the currently selected values.
+   */
+  readonly value = signal<string[]>([]);
+
+  /**
+   * Signal representing the currently highlighted option.
+   */
+  readonly highlightedOption = signal('1');
+
+  /**
+   * Reference to the injected CDK Listbox instance.
+   */
+  cdkListbox = inject(CdkListbox);
+
+  /**
+   * Lifecycle hook that initializes the component.
+   * Enables the use of active descendant for the listbox.
+   */
+  ngOnInit(): void {
+    this.cdkListbox.useActiveDescendant = true;
+  }
+
+  /**
+   * Selects the currently active option and updates the listbox value.
+   */
+  selectOption(): void {
+    this.value.set(this.listKeyManager().activeItem?.value ?? []);
+    this.cdkListbox.value = this.value();
+  }
+
+  /**
+   * Moves the active item to the next option and updates the highlighted option.
+   */
+  nextOption(): void {
+    this.listKeyManager().setNextItemActive();
+    this.highlightedOption.set(this.listKeyManager().activeItem?.value ?? '');
+    this.updateHighlightedOption();
+  }
+
+  /**
+   * Moves the active item to the previous option and updates the highlighted option.
+   */
+  previousOption(): void {
+    this.listKeyManager().setPreviousItemActive();
+    this.highlightedOption.set(this.listKeyManager().activeItem?.value ?? '');
+    this.updateHighlightedOption();
+  }
+
+  /**
+   * Updates the CSS class of options to reflect the currently highlighted option.
+   */
+  updateHighlightedOption(): void {
+    this.options().forEach(option => {
+      if (option.value === this.highlightedOption()) {
+        option.element.classList.add('cdk-option-highlighted');
+      } else {
+        option.element.classList.remove('cdk-option-highlighted');
+      }
+    });
+  }
+}
---
+++ projects/primitives/src/core/components/command/command.component.ts
@@ -0,0 +1,40 @@
+import { Component, contentChild, signal } from '@angular/core';
+import { Input } from '../input/input.component';
+import { CdkTrapFocus } from '@angular/cdk/a11y';
+import { CommandOptionsComponent } from './command-options.component';
+
+/**
+ * Component representing a command input with associated options.
+ * It provides keyboard navigation and focus trapping for accessibility.
+ */
+@Component({
+  selector: 'b-command',
+  imports: [Input, CdkTrapFocus],
+  template: `<input
+      type="text"
+      b-input
+      cdkTrapFocus
+      [cdkTrapFocusAutoCapture]="true"
+      #trappedInput
+      (keydown.arrowDown)="commandOptions()?.nextOption()"
+      (keydown.arrowUp)="commandOptions()?.previousOption()"
+      (keydown.enter)="commandOptions()?.selectOption()"
+      (blur)="trappedInput.el.nativeElement.focus()" />
+    <ng-content />`,
+  host: {
+    '[style.maxHeight]': 'maxHeight()',
+  },
+})
+export class CommandComponent {
+  /**
+   * Reference to the child `CommandOptionsComponent` if present.
+   * Used to interact with the options for navigation and selection.
+   */
+  readonly commandOptions = contentChild(CommandOptionsComponent);
+
+  /**
+   * Signal representing the maximum height of the component.
+   * This can be used to control the visual appearance of the command component.
+   */
+  readonly maxHeight = signal('300px');
+}
