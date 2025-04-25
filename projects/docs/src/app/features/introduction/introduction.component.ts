a420afa452ac689c2a1485b58ad21d3a1d13e41f
molmos <molmos@circontrol.com>
2025-04-25 19:57:21 +0200

    feat: add input and button components to introduction section for user interaction

--- projects/docs/src/app/features/introduction/introduction.component.ts
+++ projects/docs/src/app/features/introduction/introduction.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Button, Input } from '@basis-ng/primitives';
 
 @Component({
   selector: 'article[app-introduction]',
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css',
+  imports: [Input, Button],
 })
 export default class IntroductionComponent {}
