06c44ea3d0ccc021d6df373b94a52c13200d4473
molmos <molmos@circontrol.com>
2025-04-25 12:34:12 +0200

    feat: enhance CommandComponent with responsive design support and focus logic

--- projects/primitives/src/core/components/command/command.component.ts
+++ projects/primitives/src/core/components/command/command.component.ts
@@ -5,10 +5,12 @@ import {
   ElementRef,
   inject,
   input,
+  signal,
 } from '@angular/core';
 import { Input } from '../input/input.component';
 import { CdkTrapFocus } from '@angular/cdk/a11y';
 import { CommandOptionsComponent } from './command-options.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 /**
  * Component representing a command input with associated options.
@@ -21,12 +23,12 @@ import { CommandOptionsComponent } from './command-options.component';
       type="text"
       b-input
       cdkTrapFocus
-      [cdkTrapFocusAutoCapture]="true"
+      [cdkTrapFocusAutoCapture]="isDesktop()"
       #trappedInput
       (keydown.arrowDown)="commandOptions()?.nextOption()"
       (keydown.arrowUp)="commandOptions()?.previousOption()"
       (keydown.enter)="commandOptions()?.selectOption()"
-      (blur)="trappedInput.el.nativeElement.focus()" />
+      (blur)="isDesktop() && trappedInput.el.nativeElement.focus()" />
     <ng-content />`,
   host: {
     '[style.maxWidth]': 'maxWidth()',
@@ -63,6 +65,19 @@ export class CommandComponent {
    * This is linked to the options of the `CommandOptionsComponent`.
    */
   readonly options = computed(() => this.commandOptions()?.options());
+
+  /**
+   * Reference to the BreakpointObserver service for responsive design.
+   */
+  breakpointObserver = inject(BreakpointObserver);
+
+  /**
+   * Signal representing whether the current viewport is desktop or not.
+   * This is determined by checking if the Handset breakpoint is matched.
+   */
+  readonly isDesktop = signal(
+    !this.breakpointObserver.isMatched(Breakpoints.Handset)
+  );
 
   /**
    * Reference to the host element of the component.
