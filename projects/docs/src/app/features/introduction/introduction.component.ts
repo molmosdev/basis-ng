39eeaa4765fd1fbdc248b05c65c038697480b71c
molmosdev <hello@molmos.dev>
2025-04-27 22:49:28 +0200

    Actualizar imports en introduction.component.ts para eliminar dependencias innecesarias

--- projects/docs/src/app/features/introduction/introduction.component.ts
+++ projects/docs/src/app/features/introduction/introduction.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { Button, Input } from '@basis-ng/primitives';
 
 @Component({
   selector: 'article[app-introduction]',
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.css',
-  imports: [Input, Button],
+  imports: [],
 })
 export default class IntroductionComponent {}
