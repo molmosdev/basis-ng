11a173f34dbf02bd74d577dde0fc0faa2a8cb272
molmos <molmos@circontrol.com>
2025-05-02 19:16:31 +0200

    refactor: remove bottom sheet trigger button and related navigation service

--- projects/docs/src/app/core/components/header/header.component.ts
+++ projects/docs/src/app/core/components/header/header.component.ts
@@ -6,7 +6,6 @@ import { Button, Icon, MenuTrigger } from '@basis-ng/primitives';
 import { Menu } from '../../../../../../primitives/src/core/components/menu/menu.component';
 import { MenuItemRadioComponent } from '../../../../../../primitives/src/core/components/menu/shared/components/menu-item-radio/menu-item-radio.component';
 import { MenuGroupComponent } from '../../../../../../primitives/src/core/components/menu/shared/components/menu-group/menu-group.component';
-import { NavigationService } from '../navigation/navigation.service';
 
 @Component({
   selector: 'header',
@@ -28,10 +27,6 @@ export class HeaderComponent {
   responsiveService = inject(ResponsiveService);
   readonly isDesktop = computed(
     () => this.responsiveService.currentDevice() === 'desktop'
-  );
-  navigationService = inject(NavigationService);
-  readonly isBottomSheetTriggerVisible = computed(
-    () => !this.navigationService.isHomePath()
   );
 
   readonly theme = computed(() => this.themeService.theme());
