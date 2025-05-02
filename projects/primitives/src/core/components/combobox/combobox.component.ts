c52ecd7004a6daa80b2ffb609fd7dc9420669f0e
molmosdev <hello@molmos.dev>
2025-05-02 02:01:06 +0200

    refactor: replace Angular CDK overlay with custom overlay directives in combobox component

--- projects/primitives/src/core/components/combobox/combobox.component.ts
+++ projects/primitives/src/core/components/combobox/combobox.component.ts
@@ -1,4 +1,3 @@
-import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
 import {
   Component,
   computed,
@@ -16,6 +15,8 @@ import { Icon } from '../icon/icon.component';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { forwardRef } from '@angular/core';
 import { CommandComponent } from '../command/command.component';
+import { OverlayTriggerDirective } from '../../directives/overlay-trigger.directive';
+import { OverlayDirective } from '../../directives/overlay.directive';
 
 /**
  * Component representing a combobox dropdown.
@@ -23,45 +24,30 @@ import { CommandComponent } from '../command/command.component';
  */
 @Component({
   selector: 'b-combobox',
-  imports: [Button, Icon, CdkConnectedOverlay, CdkOverlayOrigin],
+  imports: [Button, Icon, OverlayTriggerDirective, OverlayDirective],
   template: ` <button
       b-button
       variant="outlined"
-      (click)="isOpen() ? close() : open()"
-      (keydown.arrowDown)="!isOpen() && open()"
-      (keydown.arrowUp)="!isOpen() && open()"
-      cdkOverlayOrigin
+      (click)="isOpen.set(!isOpen())"
+      (keydown.arrowUp)="!isOpen() && isOpen.set(true)"
+      (keydown.arrowDown)="!isOpen() && isOpen.set(true)"
       [activeEnabled]="false"
-      #trigger="cdkOverlayOrigin">
+      bOverlayTrigger
+      #trigger="bOverlayTrigger">
       {{ content() }}
       <i b-icon icon="ChevronDown" [size]="20"></i>
     </button>
     <ng-template
-      cdkConnectedOverlay
-      [cdkConnectedOverlayOrigin]="trigger"
-      [cdkConnectedOverlayOpen]="isOpen()"
-      [cdkConnectedOverlayMinWidth]="buttonWidth()"
-      [cdkConnectedOverlayHasBackdrop]="true"
-      cdkConnectedOverlayBackdropClass="cdk-overlay-transparent-backdrop"
-      [cdkConnectedOverlayPositions]="[
-        {
-          originX: 'start',
-          originY: 'bottom',
-          overlayX: 'start',
-          overlayY: 'top',
-          offsetY: 5,
-        },
-        {
-          originX: 'start',
-          originY: 'top',
-          overlayX: 'start',
-          overlayY: 'bottom',
-          offsetY: -5,
-        },
-      ]"
-      (backdropClick)="close()"
+      bOverlay
+      [trigger]="trigger"
+      [open]="isOpen()"
+      [minWidth]="buttonWidth()"
+      [positions]="['bottom-left', 'bottom-right', 'top-left', 'top-right']"
+      [closeDelay]="closeDelay()"
+      [offset]="offset()"
       (attach)="onOverlayAttached()"
-      (detach)="close()">
+      (outsideClick)="isOpen.set(false)"
+      (detach)="isOpen.set(false)">
       <ng-content />
     </ng-template>`,
   host: {
@@ -81,6 +67,11 @@ export class ComboboxComponent implements OnInit, ControlValueAccessor {
    * Defaults to 'Select an option'.
    */
   readonly placeholder = input<string>('Select an option');
+
+  /**
+   * Input for the offset value between the button and the dropdown.
+   */
+  readonly offset = input(5);
 
   /**
    * Signal indicating whether the dropdown is currently open.
@@ -150,12 +141,6 @@ export class ComboboxComponent implements OnInit, ControlValueAccessor {
   readonly closeDelay = signal(150);
 
   /**
-   * Reference to the CdkConnectedOverlay directive.
-   * This is used to manage the positioning and visibility of the dropdown overlay.
-   */
-  readonly cdkConnectedOverlay = viewChild(CdkConnectedOverlay);
-
-  /**
    * Lifecycle hook that is called after the component is initialized.
    * It sets up the necessary subscriptions for handling value changes.
    */
@@ -172,37 +157,10 @@ export class ComboboxComponent implements OnInit, ControlValueAccessor {
     this.command()
       ?.commandOptions()
       ?.closeEmitter.subscribe(() => {
-        this.onChange(this.value()!); // Notify Angular Forms about the change
-        this.onTouched(); // Mark the component as touched
-        this.close(); // Close the dropdown
+        this.onChange(this.value()!);
+        this.onTouched();
+        this.isOpen.set(false);
       });
-  }
-
-  /**
-   * Opens the dropdown and focuses the listbox.
-   * This method sets the `isOpen` signal to `true` and ensures the listbox gains focus.
-   */
-  open() {
-    this.isOpen.set(true);
-  }
-
-  /**
-   * Closes the dropdown with a transition effect and refocuses the button.
-   * This method sets the `isOpen` signal to `false` after a delay and removes
-   * the transition class from the overlay panel.
-   */
-  close() {
-    this.cdkConnectedOverlay()?.overlayRef.addPanelClass(
-      'cdk-overlay-pane-closing'
-    );
-
-    setTimeout(() => {
-      this.isOpen.set(false);
-      this.cdkConnectedOverlay()?.overlayRef.removePanelClass(
-        'cdk-overlay-pane-closing'
-      );
-      this.button()?.el.nativeElement.focus();
-    }, this.closeDelay());
   }
 
   /**
