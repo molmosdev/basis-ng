ec1fefd61e0091ac961c136a3c7e3c1d5add1443
molmosdev <hello@molmos.dev>
2025-05-03 18:53:40 +0200

    feat: add lazy loading example and keyboard navigation instructions to TabsDocumentationComponent

--- projects/docs/src/app/features/documentation/pages/components/pages/tabs-documentation.component.ts
+++ projects/docs/src/app/features/documentation/pages/components/pages/tabs-documentation.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
-import { TabsComponent, TabComponent } from '@basis-ng/primitives';
+import {
+  TabsComponent,
+  TabComponent,
+} from '../../../../../../../../primitives/src/public-api';
 import { CodeBlockComponent } from '../shared/components/code-block.component';
 import { FormsModule } from '@angular/forms';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'article[app-tabs-documentation]',
@@ -17,6 +21,12 @@ import { FormsModule } from '@angular/forms';
       without it.</span
     >
     <code-block [code]="stylesImport" />
+
+    <h2>Keyboard Navigation</h2>
+    <span>
+      Use the arrow keys to navigate between tabs. The component supports
+      horizontal navigation.
+    </span>
 
     <h2>Basic Usage</h2>
     <code-block [code]="basicUsage" />
@@ -41,13 +51,47 @@ import { FormsModule } from '@angular/forms';
       }
     </div>
 
-    <h2>Keyboard Navigation</h2>
-    <span>
-      Use the arrow keys to navigate between tabs. The component supports
-      horizontal navigation.
-    </span>`,
+    <h2>Lazy Loading Usage</h2>
+    <span> Use <code>ng-template</code> to lazily load tab content. </span>
+    <code-block [code]="lazyLoadingUsage" />
+    <div
+      class="documentation-playground"
+      style="flex-direction: column; align-items: center;">
+      <b-tabs [(ngModel)]="lazySelectedTab">
+        <b-tab value="tab1">Tab 1</b-tab>
+        <b-tab value="tab2">Tab 2</b-tab>
+        <b-tab value="tab3">Tab 3</b-tab>
+      </b-tabs>
+      @switch (lazySelectedTab[0]) {
+        @case ('tab1') {
+          <ng-container *ngTemplateOutlet="tab1Content" />
+        }
+        @case ('tab2') {
+          <ng-container *ngTemplateOutlet="tab2Content" />
+        }
+        @case ('tab3') {
+          <ng-container *ngTemplateOutlet="tab3Content" />
+        }
+      }
+    </div>
+
+    <ng-template #tab1Content>
+      <p>Lazy-loaded content for Tab 1</p>
+    </ng-template>
+    <ng-template #tab2Content>
+      <p>Lazy-loaded content for Tab 2</p>
+    </ng-template>
+    <ng-template #tab3Content>
+      <p>Lazy-loaded content for Tab 3</p>
+    </ng-template> `,
   standalone: true,
-  imports: [TabsComponent, TabComponent, CodeBlockComponent, FormsModule],
+  imports: [
+    TabsComponent,
+    TabComponent,
+    CodeBlockComponent,
+    FormsModule,
+    NgTemplateOutlet,
+  ],
 })
 export default class TabsDocumentationComponent {
   angularImport = `import { TabsComponent, TabComponent } from '@basis-ng/primitives';`;
@@ -74,4 +118,34 @@ export default class TabsDocumentationComponent {
 }`;
 
   selectedTab = ['tab2'];
+
+  lazyLoadingUsage = `<b-tabs [(ngModel)]="lazySelectedTab">
+  <b-tab value="tab1">Tab 1</b-tab>
+  <b-tab value="tab2">Tab 2</b-tab>
+  <b-tab value="tab3">Tab 3</b-tab>
+</b-tabs>
+
+@switch (lazySelectedTab[0]) {
+  @case ('tab1') {
+    <ng-container *ngTemplateOutlet="tab1Content"></ng-container>
+  }
+  @case ('tab2') {
+    <ng-container *ngTemplateOutlet="tab2Content"></ng-container>
+  }
+  @case ('tab3') {
+    <ng-container *ngTemplateOutlet="tab3Content"></ng-container>
+  }
+}
+
+<ng-template #tab1Content>
+  <p>Lazy-loaded content for Tab 1</p>
+</ng-template>
+<ng-template #tab2Content>
+  <p>Lazy-loaded content for Tab 2</p>
+</ng-template>
+<ng-template #tab3Content>
+  <p>Lazy-loaded content for Tab 3</p>
+</ng-template>`;
+
+  lazySelectedTab = ['tab1'];
 }
