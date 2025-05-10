382353a06173b6ebceb870c10b17c23eba170169
molmosdev <hello@molmos.dev>
2025-05-10 17:50:01 +0200

    feat: add maxWidth input property to InputGroupComponent for customizable width

--- projects/primitives/src/core/components/input-group/input-group.component.ts
+++ projects/primitives/src/core/components/input-group/input-group.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'b-input-group',
   template: `<ng-content />`,
+  host: {
+    '[style.max-width]': 'maxWidth()',
+  },
 })
-export class InputGroupComponent {}
+export class InputGroupComponent {
+  /**
+   * Sets the maximum width of the input group (e.g. 300px, 100%).
+   */
+  readonly maxWidth = input('100%');
+}
