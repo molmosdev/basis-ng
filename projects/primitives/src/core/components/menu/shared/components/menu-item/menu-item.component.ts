f911d7a70078d2b4d12122783d775f9e3ed0536b
molmosdev <hello@molmos.dev>
2025-05-11 12:22:43 +0200

    feat: update package versions and enhance menu item component with squared state

--- projects/primitives/src/core/components/menu/shared/components/menu-item/menu-item.component.ts
+++ projects/primitives/src/core/components/menu/shared/components/menu-item/menu-item.component.ts
@@ -1,5 +1,5 @@
 import { CdkMenuItem } from '@angular/cdk/menu';
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 /**
  * Represents a menu item with support for typeahead and disabled states.
@@ -18,5 +18,13 @@ import { Component } from '@angular/core';
       outputs: ['cdkMenuItemTriggered: triggered'],
     },
   ],
+  host: {
+    '[class.squared]': 'squared()',
+  },
 })
-export class MenuItemComponent {}
+export class MenuItemComponent {
+  /**
+   * Input to set the squared state of the menu item.
+   */
+  readonly squared = input(false);
+}
