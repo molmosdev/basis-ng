da94f58bc84343db094654c9a0f27d9b6b613993
molmosdev <hello@molmos.dev>
2025-09-10 23:42:46 +0200

    fix(header): update host class for sticky positioning

--- projects/docs/src/app/core/components/header.ts
+++ projects/docs/src/app/core/components/header.ts
@@ -20,7 +20,7 @@ import { RouterLink } from '@angular/router';
     </svg>
   `,
   host: {
-    class: 'h-20 px-8 flex items-center absolute top-0 left-0 right-0',
+    class: 'h-20 px-8 flex items-center sticky top-0 z-10',
   },
 })
 export class Header {}
