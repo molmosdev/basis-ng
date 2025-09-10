7456de227a6944d418d37f4940ce489ab84f3b1e
molmosdev <hello@molmos.dev>
2025-09-10 21:00:17 +0200

    refactor(header): remove unused CSS and update component imports

--- projects/docs/src/app/core/components/header/header.component.ts
+++ projects/docs/src/app/core/components/header/header.component.ts
@@ -3,25 +3,28 @@ import { RouterLink } from '@angular/router';
 import { ThemeService } from '../../../../../../primitives/src/core/services/theme.service';
 import { ResponsiveService } from '../../../../../../primitives/src/core/services/responsive.service';
 import {
-  ButtonComponent,
+  Button,
   MenuComponent,
   MenuTriggerDirective,
 } from '@basis-ng/primitives';
 import { MenuItemRadioComponent } from '../../../../../../primitives/src/core/components/menu/shared/components/menu-item-radio/menu-item-radio.component';
 import { MenuGroupComponent } from '../../../../../../primitives/src/core/components/menu/shared/components/menu-group/menu-group.component';
+import { provideIcons, NgIcon } from '@ng-icons/core';
+import { lucideMoon, lucideSun, lucideSunMoon } from '@ng-icons/lucide';
 
 @Component({
   selector: 'header',
   imports: [
     RouterLink,
-    ButtonComponent,
+    Button,
     MenuTriggerDirective,
     MenuComponent,
     MenuItemRadioComponent,
     MenuGroupComponent,
+    NgIcon,
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  providers: [provideIcons({ lucideMoon, lucideSun, lucideSunMoon })],
 })
 export class HeaderComponent {
   themeService = inject(ThemeService);
@@ -29,7 +32,6 @@ export class HeaderComponent {
   readonly isDesktop = computed(
     () => this.responsiveService.currentDevice() === 'desktop'
   );
-
   readonly theme = computed(() => this.themeService.theme());
 
   applyTheme(theme: 'light' | 'dark' | 'auto'): void {
