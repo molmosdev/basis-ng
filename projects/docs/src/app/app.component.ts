c6237c85a85d9be50324e1a121e1db78c819ab25
molmosdev <hello@molmos.dev>
2025-09-10 02:56:07 +0200

    refactor(app): clean up unused imports and signals for improved readability

--- projects/docs/src/app/app.component.ts
+++ projects/docs/src/app/app.component.ts
@@ -2,27 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from '../../../primitives/src/core/services/theme.service';
 import { HeaderComponent } from './core/components/header/header.component';
-import { OverlayTriggerDirective } from 'projects/primitives/src/core/directives/overlay-trigger.directive';
-import { SelectTrigger } from 'projects/primitives/src/core/components/select-v2/select/components/select-trigger';
-import { SelectContent } from 'projects/primitives/src/core/components/select-v2/select/components/select-content';
-import { Select } from 'projects/primitives/src/core/components/select-v2/select/select';
-import { SelectValue } from 'projects/primitives/src/core/components/select-v2/select/components/select-value';
-import { Option } from 'projects/primitives/src/shared/components/option.component';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    OverlayTriggerDirective,
-    Select,
-    SelectTrigger,
-    SelectValue,
-    SelectContent,
-    Option,
-    FormsModule,
-  ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -31,16 +14,4 @@ export class AppComponent {
   themeService = inject(ThemeService);
   readonly isMenuVisible = signal(false);
   readonly isThemeConfigVisible = signal(false);
-  // Signal para controlar el overlay original
-  readonly isOverlayOpen = signal(false);
-
-  // Signals y métodos para los overlays de prueba
-  readonly isOverlayOpen1 = signal(false);
-  readonly isOverlayOpen2 = signal(false);
-
-  closeOverlay1 = () => this.isOverlayOpen1.set(false);
-  closeOverlay2 = () => this.isOverlayOpen2.set(false);
-
-  //  new select
-  example: string[] = ['option1'];
 }
