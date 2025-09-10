76fa14b00b0038db8374b8f78590a3364fd4d8b1
molmosdev <hello@molmos.dev>
2025-09-10 21:00:41 +0200

    refactor(app): remove unused CSS and HTML, simplify component structure

--- projects/docs/src/app/app.routes.ts
+++ projects/docs/src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
   },
   {
     path: 'home',
-    loadComponent: () => import('./features/home/home.component'),
+    loadComponent: () => import('./features/home.component'),
     title: 'Basis UI',
   },
   {
