38bf3061e54b85c50deef1d8b54864750416114a
molmosdev <hello@molmos.dev>
2025-09-11 00:13:00 +0200

    fix(code-block): adjust button size and icon size for improved UI consistency

--- projects/docs/src/app/features/documentation/pages/components/shared/components/code-block.component.ts
+++ projects/docs/src/app/features/documentation/pages/components/shared/components/code-block.component.ts
@@ -10,14 +10,14 @@ import { HighlightAuto } from 'ngx-highlightjs';
   template: `
     <pre
       class="flex relative rounded-lg overflow-auto inset-ring-1 inset-ring-secondary dark:inset-ring-secondary-dark">
-      <code class="!bg-secondary/5 !dark:bg-secondary-dark/10 p-2 min-w-full" [highlightAuto]="code()"></code>
+      <code class="!bg-secondary/5 text-sm !dark:bg-secondary-dark/10 p-2 min-w-full" [highlightAuto]="code()"></code>
       <button
       b-button
-      size="md"
+      size="sm"
       class="absolute top-2 right-2"
       variant="ghost"
       [squared]="true">
-        <ng-icon name="lucideCopy" size="18" color="currentColor" />
+        <ng-icon name="lucideCopy" size="14" color="currentColor" />
       </button>
     </pre>
   `,
