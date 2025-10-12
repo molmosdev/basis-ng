83b1385ac1c843d4ec83cdfe93f99fcafb306d96
molmosdev <hello@molmos.dev>
2025-10-12 23:23:13 +0200

    chore: update dependencies and ESLint configurations
    - Updated `eslint-plugin-unused-imports` to `eslint-config-prettier` and `eslint-plugin-prettier` in package.json.
    - Upgraded `angular-eslint` to version 20.4.0.
    - Updated `typescript-eslint` to version 8.46.0.
    - Refactored ESLint configuration files in projects/docs and projects/primitives for consistency and readability.

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
 // @ts-check
-const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
-const unusedImports = require('eslint-plugin-unused-imports');
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
 const prettier = require('eslint-config-prettier');
 const prettierPlugin = require('eslint-plugin-prettier');
 
+
 module.exports = tseslint.config(
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -17,31 +17,16 @@ module.exports = tseslint.config(
       prettier,
     ],
     plugins: {
-      'unused-imports': unusedImports,
       prettier: prettierPlugin,
     },
     processor: angular.processInlineTemplates,
-    rules: {
-      '@angular-eslint/prefer-signals': 'error',
-      '@angular-eslint/component-class-suffix': ['off'],
-      '@angular-eslint/directive-class-suffix': ['off'],
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          printWidth: 100,
-          tabWidth: 2,
-        },
-      ],
-    },
   },
   {
-    files: ['**/*.html'],
+    files: ["**/*.html"],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
-      prettier,
     ],
     rules: {},
-  },
+  }
 );
