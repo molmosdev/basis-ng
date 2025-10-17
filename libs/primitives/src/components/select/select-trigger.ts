29870f9fde131fe4d2d17b1f01efc49845e046a9
molmosdev <hello@molmos.dev>
2025-10-17 17:44:38 +0200

    refactor: replace SVG icon with ng-icon for select trigger component

--- libs/primitives/src/components/select/select-trigger.ts
+++ libs/primitives/src/components/select/select-trigger.ts
@@ -1,33 +1,23 @@
 import { Component, ElementRef, inject, output, signal } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideAArrowDown, lucideAArrowUp } from '@ng-icons/lucide';
 
 /**
  * A button that acts as the trigger for a select dropdown.
  */
 @Component({
   selector: 'button[b-select-trigger]',
+  imports: [NgIcon],
   template: `
     <ng-content />
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-chevron-down-icon lucide-chevron-down"
-    >
-      <path d="m6 9 6 6 6-6" />
-    </svg>
+    <ng-icon [name]="triggered() ? 'lucide-a-arrow-up' : 'lucide-a-arrow-down'" />
   `,
   host: {
-    '(keydown.arrowUp)': 'buttonClicked.emit()',
-    '(keydown.arrowDown)': 'buttonClicked.emit()',
-    '(click)': 'buttonClicked.emit()',
+    '(keydown)': 'handleKeydown($event)',
+    '(click)': 'handleClick()',
     '[disabled]': 'disabled()',
   },
+  providers: [provideIcons({ lucideAArrowDown, lucideAArrowUp })],
 })
 export class SelectTrigger {
   /**
@@ -44,4 +34,28 @@ export class SelectTrigger {
    * Whether the trigger is disabled.
    */
   readonly disabled = signal(false);
+
+  /**
+   * Whether the trigger is activated.
+   */
+  triggered = signal<boolean>(false);
+
+  /**
+   * Handle click events on the trigger.
+   */
+  handleClick(): void {
+    this.buttonClicked.emit();
+    this.triggered.set(!this.triggered());
+  }
+
+  /**
+   * Handle keydown events on the trigger.
+   * @param event - The keyboard event.
+   */
+  handleKeydown(event: KeyboardEvent): void {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+      this.buttonClicked.emit();
+      this.triggered.set(event.key === 'ArrowUp');
+    }
+  }
 }
